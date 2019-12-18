d9241f56611150a8449551cc12fdd669e8f25104
Elise Guezel <elise.guezel@yahoo.fr>
2019-12-18 15:43:18 +0100

    Create static map in HereMap

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,32 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+    <div id="app">
+        <HereMap
+            appId="DiGvwlJhXUFwnnkS1DL0"
+            appCode="1sKutluykr4HfVFdV-BKJZ77Sz1SSWTNW3-WF6Lir18"
+            lat="47.2205"
+            lng="-1.5600"
+            width="100%"
+            height="835px" />
+    </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+    import HereMap from "./components/HereMap.vue"
 
-export default {
-  name: 'app',
-  components: {
-    HelloWorld
-  }
-}
+    export default {
+        name: 'app',
+        components: {
+            HereMap
+        }
+    }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+    #app {
+        font-family: 'Avenir', Helvetica, Arial, sans-serif;
+        -webkit-font-smoothing: antialiased;
+        -moz-osx-font-smoothing: grayscale;
+        text-align: center;
+        color: #2c3e50;
+    }
 </style>
