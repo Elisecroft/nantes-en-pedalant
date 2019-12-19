31fd54c58eea3532110a81bd11d55fade4436f24
Elise Guezel <elise.guezel@yahoo.fr>
2019-12-19 07:28:03 +0100

    Make interractive map with leaflet

--- src/App.vue
+++ src/App.vue
@@ -1,44 +1,18 @@
 <template>
   <div id="app">
-    <HereMap
-      ref="map"
-      apiKey=""
-      lat="47.2205"
-      lng="-1.5600"
-      zoom="13" />
+    <Map />
   </div>
 </template>
 
 <script>
-  let proxySettings = {
-    proxyType: "system",
-    http: "http://pubproxy.com/api/proxy?api=123",
-    passthrough: "127.0.0.1"
-  };
+import Map from './components/Map.vue'
 
-  browser.proxy.settings.set({value: proxySettings});
-
-  import HereMap from "./components/HereMap.vue"
-
-  export default {
-    name: 'app',
-    components: {
-      HereMap
-    },
-    mounted() {
-      let map = this.$refs.map;
-      map.drawRoute(
-        {
-          lat: "48",
-          lng: "-1.4"
-        },
-        {
-          lat: "47.8",
-          lng: "-1.3"
-        }
-      )
-    } 
-  }
+export default {
+  name: 'app',
+  components: {
+    Map,
+  },
+}
 </script>
 
 <style>
---
+++ src/components/Map.vue
@@ -0,0 +1,58 @@
+<template>
+  <div style="height: 100vh; width: 100vw">
+    <LMap
+      style="height: 100%; width: 100%"
+      :zoom="zoom"
+      :center="center"
+      @update:zoom="zoomUpdated"
+      @update:center="centerUpdated"
+      @update:bounds="boundsUpdated"
+    >
+      <LTileLayer :url="url"></LTileLayer>
+    </LMap>
+  </div>
+</template>
+
+<script>
+import { LMap, LTileLayer } from "vue2-leaflet";
+
+import Vue from "vue";
+import "leaflet/dist/leaflet.css";
+
+Vue.component("l-map", LMap);
+Vue.component("l-tile-layer", LTileLayer);
+
+export default {
+  data() {
+    return {
+      url:
+        "https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoiZWxpc2Vjcm9mdCIsImEiOiJjazRjY2FxamowNDN6M21vMXBpaGV1bXNhIn0.ZlAf35jELFpyEr3pR0Lx1A",
+      zoom: 13,
+      center: [47.213039, -1.549931], //GMap
+      markerLatLng: [47.216303, -1.550231],
+    };
+  },
+  methods: {
+    zoomUpdated(zoom) {
+      this.zoom = zoom;
+    },
+    centerUpdated(center) {
+      this.center = center;
+    },
+    boundsUpdated(bounds) {
+      this.bounds = bounds;
+    }
+  },
+  name: "Map",
+  components: {
+    LMap,
+    LTileLayer
+  }
+};
+</script>
+
+
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
